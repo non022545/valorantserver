e663288a489767bc83fbaf079ff4eb28e0b97a41
non022545 <nontthont7@gmail.com>
2025-05-17 16:58:22 +0700

    connect

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ console.log('MYSQL_PORT:', process.env.MYSQL_PORT);
 
 
 const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
   ssl: false
 });
 
