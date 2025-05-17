b5d51664f078be609acfa27c3881370a560f4ffa
non022545 <nontthont7@gmail.com>
2025-05-17 16:54:26 +0700

    sql

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const mysql = require('mysql2')
 const cors = require('cors')
-require('dotenv').config();
 
 app.use(cors())
 app.use(express.json())
+
+console.log('MYSQL_HOST:', process.env.MYSQL_HOST);
+console.log('MYSQL_USER:', process.env.MYSQL_USER);
+console.log('MYSQL_PASSWORD:', process.env.MYSQL_PASSWORD ? '******' : null);
+console.log('MYSQL_DATABASE:', process.env.MYSQL_DATABASE);
+console.log('MYSQL_PORT:', process.env.MYSQL_PORT);
+
 
 const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
